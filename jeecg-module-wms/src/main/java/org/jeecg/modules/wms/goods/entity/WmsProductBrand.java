58198d7c0146934bb8348beb951a7b101eea7697
JhWuuu <1132240632gwen@gmail.com>
2026-06-01 10:02:58 +0800

    分类编码商品信息下拉框父子节点显示

--- jeecg-module-wms/src/main/java/org/jeecg/modules/wms/goods/entity/WmsProductBrand.java
+++ jeecg-module-wms/src/main/java/org/jeecg/modules/wms/goods/entity/WmsProductBrand.java
@@ -1,23 +1,19 @@
 package org.jeecg.modules.wms.goods.entity;
 
-import java.io.Serializable;
-import java.io.UnsupportedEncodingException;
-import java.util.Date;
-import java.math.BigDecimal;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.TableLogic;
-import org.jeecg.common.constant.ProvinceCityArea;
-import org.jeecg.common.util.SpringContextUtils;
-import lombok.Data;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.jeecgframework.poi.excel.annotation.Excel;
-import org.jeecg.common.aspect.annotation.Dict;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+import org.jeecg.common.aspect.annotation.Dict;
+import org.jeecgframework.poi.excel.annotation.Excel;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @Description: 商品品牌
@@ -66,6 +62,7 @@ public class WmsProductBrand implements Serializable {
     private String logo;
 	/**状态*/
 	@Excel(name = "状态", width = 15)
+    @Dict(dicCode = "wms_status")
     @Schema(description = "状态")
     private String status;
 }
--- jeecg-module-wms/src/main/java/org/jeecg/modules/wms/goods/entity/WmsProductCategories.java
+++ jeecg-module-wms/src/main/java/org/jeecg/modules/wms/goods/entity/WmsProductCategories.java
@@ -1,22 +1,18 @@
 package org.jeecg.modules.wms.goods.entity;
 
-import java.io.Serializable;
-import java.util.Date;
-import java.math.BigDecimal;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.TableLogic;
-import org.jeecg.common.constant.ProvinceCityArea;
-import org.jeecg.common.util.SpringContextUtils;
-import lombok.Data;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.jeecgframework.poi.excel.annotation.Excel;
-import org.jeecg.common.aspect.annotation.Dict;
-
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.io.UnsupportedEncodingException;
+import lombok.Data;
+import org.jeecg.common.aspect.annotation.Dict;
+import org.jeecgframework.poi.excel.annotation.Excel;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @Description: 商品类别
@@ -75,4 +71,7 @@ public class WmsProductCategories implements Serializable {
 	@Excel(name = "节点编码", width = 15)
     @Schema(description = "节点编码")
     private String categoryCode;
+    /**父节点名称用于显示*/
+    @TableField(exist = false)
+    private String parentName;
 }
